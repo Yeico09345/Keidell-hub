6fe96ae7448e60d5c92ddacf366dc5188e881fc0
Yeico09345 <1285532@g.essextech.org>
2025-09-09 02:46:21 -0400

    Update Keidellhub.lua

--- Keidellhub.lua
+++ Keidellhub.lua
@@ -1,3 +1,5 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    LoadingTitle = "Rayfield Interface Suite",
