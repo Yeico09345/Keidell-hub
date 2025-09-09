06179b8d1b3c3f5c6468cb49cfdd3ed2eabdaa19
Yeico09345 <1285532@g.essextech.org>
2025-09-09 02:38:14 -0400

    Update Keidellhub.lua

--- Keidellhub.lua
+++ Keidellhub.lua
@@ -1,3 +1,5 @@
+local rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "Keidell hub 👻 ",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
